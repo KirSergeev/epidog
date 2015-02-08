17a235e89149dbb0914a78cf16bd2a39b34fc0ec
KirSergeev <kir.sergeev@gmail.com>
2015-02-08 23:36:41 +0100

    New

---
+++ api/v1/config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_HOST', 		'u47838.mysql.masterhost.ru');
+define('DB_USERNAME', 	'u47838');
+define('DB_PASSWORD', 	'4Nt.OnfRAPO4.t');
+define('DB_NAME', 		'u47838');
